bf982bd17451332d839f82808df9b98406ee9877
networmix <networmix@gmail.com>
2019-06-03 00:45:13 +0100

    Basic URL shortener with simple tests

--- hw1_url/urlshortner.go
+++ hw1_url/urlshortner.go
@@ -2,26 +2,79 @@ package urlshortener
 
 import (
 	"crypto/md5"
+	"encoding/base64"
 	"fmt"
 	"net/url"
 )
 
-// Shortener is a simple URL shortner.
-type Shortener struct {
+// Shortener is the interface of a URL shortener.
+// Shorten - accepts a string and, if that string is a valid URL, it returns a shortened URL.
+// Resolve - accepts a string and, if that string corresponds to a cached URL, it returns the previously memorised URL.
+type Shortener interface {
+	Shorten(url string) string
+	Resolve(url string) string
+}
+
+// MD5Shortener is a simple URL shortner which uses MD5 to shorten supplied URLs.
+type MD5Shortener struct {
 	urls map[string]string
 }
 
-func (s *Shortener) Shorten(url string) string {
+// Shorten method accepts accepts a string URL and, if the string is a valid URL, it returns a shortened URL.
+// If URL is not valid, the method returns an empty string.
+func (s *MD5Shortener) Shorten(url string) string {
 	if checkIfURLisValid(url) {
 		urlBytes := []byte(url)
 		urlHash := md5.Sum(urlBytes)
 		shortURL := fmt.Sprintf("%x", urlHash[:8])
-		s.urls[url] = shortURL
+		s.urls[shortURL] = url
 		return shortURL
 	}
 	return ""
 }
 
+// Resolve expects a string with a short url and returns corresponding cached long url (if any) or an empty string.
+func (s *MD5Shortener) Resolve(url string) string {
+	originalURL := s.urls[url]
+	return originalURL
+}
+
+// BASE64Shortener is a simple URL shortner which uses BASE64 to shorten supplied URLs.
+type BASE64Shortener struct {
+	urls map[string]string
+}
+
+func (s *BASE64Shortener) getNextID() int {
+	return len(s.urls)
+}
+
+// Shorten method accepts accepts a string URL and, if the string is a valid URL, it returns a shortened URL.
+// If URL is not valid, the method returns an empty string.
+func (s *BASE64Shortener) Shorten(url string) string {
+	if checkIfURLisValid(url) {
+		idBytes := []byte(string(s.getNextID()))
+		// From RFC4648:
+		// The encoding process represents 24-bit groups of input bits as output
+		// strings of 4 encoded characters.  Proceeding from left to right, a
+		// 24-bit input group is formed by concatenating 3 8-bit input groups.
+		// These 24 bits are then treated as 4 concatenated 6-bit groups, each
+		// of which is translated into a single character in the base 64
+		// alphabet.
+		// Please look at https://tools.ietf.org/html/rfc4648#page-7 for the full reference.
+		urlEncodedIndex := base64.RawURLEncoding.EncodeToString(idBytes)
+		s.urls[urlEncodedIndex] = url
+		return urlEncodedIndex
+	}
+	return ""
+}
+
+// Resolve expects a string with a short url and returns corresponding cached long url (if any) or an empty string.
+func (s *BASE64Shortener) Resolve(url string) string {
+	originalURL := s.urls[url]
+	return originalURL
+}
+
+// checkIfURLisValid checks the validity of the URL.
 func checkIfURLisValid(rawURL string) bool {
 	_, err := url.ParseRequestURI(rawURL)
 	if err != nil {
--- hw1_url/urlshortner_test.go
+++ hw1_url/urlshortner_test.go
@@ -25,7 +25,7 @@ func Test_checkIfURLisValid(t *testing.T) {
 	}
 }
 
-func TestShortener_Shorten(t *testing.T) {
+func TestMD5Shortener_Shorten(t *testing.T) {
 	type fields struct {
 		urls map[string]string
 	}
@@ -43,12 +43,113 @@ func TestShortener_Shorten(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &Shortener{
+			s := &MD5Shortener{
 				urls: tt.fields.urls,
 			}
 			if got := s.Shorten(tt.args.url); got != tt.want {
 				t.Errorf("Shortener.Shorten() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestMD5Shortener_Resolve(t *testing.T) {
+	type fields struct {
+		urls map[string]string
+	}
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{"CachedURL", fields{map[string]string{"703a8b3ff83e3ea1": "https://gobyexample.com"}}, args{"703a8b3ff83e3ea1"}, "https://gobyexample.com"},
+		{"NotCachedURL", fields{make(map[string]string)}, args{"NotCachedURL"}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &MD5Shortener{
+				urls: tt.fields.urls,
+			}
+			if got := s.Resolve(tt.args.url); got != tt.want {
+				t.Errorf("URLShortener.Resolve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBASE64Shortener_Shorten(t *testing.T) {
+	type fields struct {
+		urls map[string]string
+	}
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{"ValidURL", fields{make(map[string]string)}, args{"https://gobyexample.com"}, "AA"},
+		{"InvalidURL", fields{make(map[string]string)}, args{"gobyexample.com"}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &BASE64Shortener{
+				urls: tt.fields.urls,
+			}
+			if got := s.Shorten(tt.args.url); got != tt.want {
+				t.Errorf("Shortener.Shorten() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBASE64Shortener_Resolve(t *testing.T) {
+	type fields struct {
+		urls map[string]string
+	}
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{"CachedURL", fields{map[string]string{"AA": "https://gobyexample.com"}}, args{"AA"}, "https://gobyexample.com"},
+		{"NotCachedURL", fields{make(map[string]string)}, args{"NotCachedURL"}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &BASE64Shortener{
+				urls: tt.fields.urls,
+			}
+			if got := s.Resolve(tt.args.url); got != tt.want {
+				t.Errorf("URLShortener.Resolve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestShortener_interface(t *testing.T) {
+	md5shortener := &MD5Shortener{
+		urls: make(map[string]string),
+	}
+	base64shortener := &BASE64Shortener{
+		urls: make(map[string]string),
+	}
+	shorteners := []Shortener{md5shortener, base64shortener}
+	testURL := "https://gobyexample.com"
+	for _, s := range shorteners {
+		shortURL := s.Shorten(testURL)
+		if got := s.Resolve(shortURL); got != testURL {
+			t.Errorf("URLShortener.Resolve() = %v, want %v", got, testURL)
+		}
 	}
 }
