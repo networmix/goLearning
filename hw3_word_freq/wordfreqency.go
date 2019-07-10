06875ae3b6691484f27d260c943dc2d241751c19
Andrey Golovanov <golova@amazon.com>
2019-07-11 01:59:09 +0300

    HomeWork3 - word freqency

---
+++ hw3_word_freq/wordfreqency.go
@@ -0,0 +1,64 @@
+package wordfreqency
+
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type KeyValue struct {
+	key   string
+	value int
+}
+
+type KeyValueList []KeyValue
+
+func (kvlist KeyValueList) Len() int {
+	return len(kvlist)
+}
+
+func (kvlist KeyValueList) Swap(i, j int) {
+    kvlist[i], kvlist[j] = kvlist[j], kvlist[i]
+}
+
+func (kvlist KeyValueList) Less(i, j int) bool {
+    if kvlist[i].value > kvlist[j].value {
+        return true
+    }
+
+    if kvlist[i].value == kvlist[j].value {
+        return sort.StringsAreSorted([]string{kvlist[i].key, kvlist[j].key})
+    }
+    return false
+}
+
+// GetTopWords takes a text and n as input. It returns a KeyValueList with n most frequent words.
+func GetTopWords(text string, n int) KeyValueList {
+	wordFrequencies := buildFreqDict(text)
+	kvlist := make(KeyValueList, len(wordFrequencies))
+	for key, value := range wordFrequencies {
+		kvlist = append(kvlist, KeyValue{key, value})
+	}
+	sort.Sort(KeyValueList(kvlist))
+	if len(kvlist) < n {
+		return kvlist
+	}
+	return kvlist[:n]
+}
+
+// buildFreqDict builds a word frequency dictionary for a given text.
+func buildFreqDict(text string) map[string]int {
+	wordFrequencies := make(map[string]int)
+	for _, word := range strings.Fields(normaliseText(text)) {
+		wordFrequencies[word] += 1
+	}
+	return wordFrequencies
+}
+
+// normaliseWords replaces any newline and punctuation symbols in a given text with spaces
+// and makes all symbols lower case.
+func normaliseText(text string) string {
+	re := regexp.MustCompile("[[:punct:]\n\t]")
+	text = re.ReplaceAllString(text, " ")
+	return strings.ToLower(text)
+}
---
+++ hw3_word_freq/wordfreqency_test.go
@@ -0,0 +1,74 @@
+package wordfreqency
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildFreqDict(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]int
+	}{
+		{"Test1", args{"OneWord"}, map[string]int{"oneword": 1}},
+		{"Test2", args{"Hello!\nWorld!"}, map[string]int{"hello": 1, "world": 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildFreqDict(tt.args.text); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetFreqDict() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_normaliseText(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Test1", args{"Hello world!"}, "hello world "},
+		{"Test2", args{"Hello!\nWorld!"}, "hello  world "},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := normaliseText(tt.args.text); got != tt.want {
+				t.Errorf("normaliseText() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetTopWords(t *testing.T) {
+	type args struct {
+		text string
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want KeyValueList
+	}{
+		{"Test1", args{"", 10}, KeyValueList{}},
+		{"Test2", args{"Hello!\n World, my bright bright world!", 3}, KeyValueList{
+			KeyValue{"bright", 2},
+			KeyValue{"world", 2},
+			KeyValue{"hello", 1},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetTopWords(tt.args.text, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetTopWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
