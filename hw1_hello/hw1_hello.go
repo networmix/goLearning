1c26905de6eb1fb10a4a9accb191f4a422a80915
Andrey Golovanov <golova@amazon.com>
2020-01-22 00:35:58 +0000

    minor tweaks to hw1_hello.go

--- hw1_hello/hw1_hello.go
+++ hw1_hello/hw1_hello.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
+	"fmt"
+	"log"
+	"time"
 
-    "github.com/beevik/ntp"
+	"github.com/beevik/ntp"
 )
 
 const (
-    ntpPool       string        = "pool.ntp.org"
-    maxRetries    int           = 5
-    sleepDuration time.Duration = 5e9
+	ntpPool       string        = "pool.ntp.org"
+	maxRetries    int           = 5
+	sleepDuration time.Duration = 5e9  // 5*10^9 nanoseconds = 5 seconds
 )
 
 // retry function retries the given function for the given number of times
 func retry(attempts int, sleep time.Duration, f func() error) (err error) {
-    for i := 0; i < attempts; i++ {
-        err = f()
-        if err == nil {
-            return err
-        }
-        log.Println("ERROR: ", err)
-        log.Printf("Error is retryable. Sleeping for %ds and retrying...\n", sleep/1e9)
-        time.Sleep(sleep)
-    }
-    return fmt.Errorf("FATAL ERROR after %d attempts, last error: %s", attempts, err)
+	for i := 0; i < attempts; i++ {
+		err = f()
+		if err == nil {
+			return err
+		}
+		log.Println("ERROR: ", err)
+		log.Printf("Error is retryable. Sleeping for %d seconds and retrying...\n", sleep/1e9)
+		time.Sleep(sleep)
+	}
+	return fmt.Errorf("FATAL ERROR after %d attempts, last error: %s", attempts, err)
 }
 
 // printNTPtime function uses a subset of NTP, called SNTP, to print current time.
+// To make our local system clock reading more accurate, we add ClockOffset,
+// which is the estimated offset relative to the server's clock.
 func printNTPtime() (err error) {
-    response, err := ntp.Query(ntpPool)
-    if err == nil {
-        time := time.Now().Add(response.ClockOffset)
-        fmt.Println(time)
-    }
-    return err
+	response, err := ntp.Query(ntpPool)
+	if err == nil {
+		curTime := time.Now().Add(response.ClockOffset)
+		fmt.Println(curTime)
+	}
+	return err
 }
 
 func main() {
-    retry(maxRetries, sleepDuration, printNTPtime)
+	retry(maxRetries, sleepDuration, printNTPtime)
 }
